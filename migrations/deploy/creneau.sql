90efecccff9c9459c6a397b6eb3fa3b07d60dc95
Anais Terrier <anais.terrier@oclock.school>
2025-07-04 16:37:55 +0200

    create db

--- migrations/deploy/creneau.sql
+++
@@ -1,7 +0,0 @@
--- Deploy jtbike:creneau to pg
-
-BEGIN;
-
--- XXX Add DDLs here.
-
-COMMIT;
---
+++ migrations/deploy/script.sql
@@ -0,0 +1,45 @@
+-- Deploy jtbike:creneau to pg
+
+BEGIN;
+
+
+CREATE TABLE "role"(
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "name" TEXT NOT NULL,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+  "updated_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "bike_user"(
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "lastname" TEXT NOT NULL,
+  "firstname" TEXT NOT NULL,
+  "email" TEXT NOT NULL UNIQUE,
+  "bike" TEXT,
+  "suspension" TEXT,
+  "password" TEXT NOT NULL,
+  "role_id" INT NOT NULL REFERENCES "role"("id") ,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+  "updated_at" TIMESTAMPTZ
+);
+
+
+CREATE TABLE "timeslot"(
+   "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   "day"  DATE NOT NULL,
+   "available" BOOLEAN DEFAULT TRUE,
+    UNIQUE(day),
+   "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+   "updated_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "reservation"(
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "bike_user_id" INT NOT NULL REFERENCES "bike_user"("id") ,
+  "timeslot_id" INT NOT NULL REFERENCES "timeslot"("id"),
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+  "updated_at" TIMESTAMPTZ
+);
+
+
+COMMIT;
